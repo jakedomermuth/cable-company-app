8a250f4027e3344903643ea88e3e8fd21620562e
Jake Domermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-09-24 15:59:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from database import create_tables, add_customers, delete_customers
+from database import create_tables, add_customers, delete_customers, customer_exist
 
 
 INTERFACE_PROMPT = """
@@ -10,13 +10,15 @@ What would you like to do?
 4. Update or Add Billing
 5. Add, Remove, And Edit Services or Equipment
 6. Exit
-Please Enter the Number For Your Desired Option:
+Please enter the number for your desired option:
 """
 
 ADD_OR_REMOVE_SELECTION = """
 What would you like to do?
 1. Add a Customer
 2. Remove a Customer
+3. Exit
+Please enter the number for your desired option:
 """
 
 
@@ -33,20 +35,39 @@ def get_customer_information():
     customer_name = input('What is the users name? ')
     customer_number = input('What is the users phone number (xxx-xxx-xxxx) ')
     customer_card_number = input('What is the users card number? ')
+    customer_name = format_input(customer_name)
     return customer_name, customer_number, customer_card_number
 
 
 def get_customer_to_delete():
-    customer_id = input('What is the id number of the customer would you like to remove?')
-    return customer_id
+    customer_name = input('What is the name of the customer would you like to remove? ')
+    customer_name = format_input(customer_name)
+    return customer_name
 
-# Utility Functions
+
+def confirm_del_user():
+    delete_choice = input(f'Are you sure you want to delete {cust_name_to_delete}? (Y/N)')
+    delete_choice = format_input(delete_choice)
+    return delete_choice
+
+# Utility Functions--------------------------------------------------------------------------------
+def format_input(name):
+    clean_name = name.strip().upper()
+    return clean_name
 
 
+# def adding_customer(customer_name, customer_number, customer_card_number):
+#     #customer_name, customer_number, customer_card_number = get_customer_information()
+#     if customer_exist(customer_name):
+#         print("This user already exist")
+#         return None
+#     else:
+#         add_customers(customer_name, customer_number, customer_card_number)
+#         ### print(f'{name} has been added to the database. Their user ID is {id}.')
 
 # Main
 def main():
-    user_choice = input(INTERFACE_PROMPT)
+    (user_choice := input(INTERFACE_PROMPT)) != '6'
     while user_choice != '6':
         if user_choice == '1':
             pass
@@ -54,12 +75,23 @@ def main():
             add_or_remove = add_remove_customers_prompt()
             if add_or_remove == '1':
                 customer_name, customer_number, customer_card_number = get_customer_information()
-                add_customers(customer_name, customer_number, customer_card_number)
-                # print inication that the user was added
+                if customer_exist(customer_name):
+                     print("This user already exist")
+                else:
+                    add_customers(customer_name, customer_number, customer_card_number)
+                    print(f'{customer_name} has been added to the database.')
             elif add_or_remove == '2':
-                cust_id_to_delete = get_customer_to_delete()
-                #return user and ask if they still want to delete
-                delete_customers(cust_id_to_delete)
+                cust_name_to_delete = get_customer_to_delete()
+                if customer_exist(cust_name_to_delete):
+                    delete_choice = input(f'Are you sure you want to delete {cust_name_to_delete}? (Y/N)')
+                    # looking to delete(need to get variable in confirm del function)
+                    if delete_choice == 'Y':
+                        delete_customers(cust_name_to_delete)
+                        print(f'{cust_name_to_delete} has been removed.')
+                    else:
+                        print('User has not been deleted.')
+                else:
+                    print("User is not in the database.")
             else:
                 break
         elif user_choice == '3':
